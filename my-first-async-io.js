8b06f2b0e7dd787d78ec17e2079ef8a08e11b108
jodiDemiri <64123665+jodiDemiri@users.noreply.github.com>
2022-03-01 00:51:51 -0600

    Delete my-first-async-io.js

--- my-first-async-io.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-    var file = fs.readFile(process.argv[2], function operation(err, file)
-    {
-        const str = file.toString()
-        splitstr = str.split('\n')
-        console.log(splitstr.length-1)
-    })  
